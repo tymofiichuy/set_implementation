26e5a7b3983db1a0a9fb87f9a0169b259bf0bf55
tymofiichuy <tymofiichuy@gmail.com>
2024-09-10 00:10:15 +0300

    change boundaries

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+
 #include "set.h"
 
 using namespace std;
+
+bool multitype_set::set_search(variants el){
+    node* temp = head;
+    while (temp->link != nullptr){
+        if (typeid(el).name() == typeid(temp->data).name()){
+            if (el == temp->data){
+                return true;
+            }
+        }
+    }
+}
--- set.h
+++ set.h
@@ -1,19 +1,22 @@
 #pragma once
-//#include<variant>
-#include<any>
+#include<variant>
+#include<string>
+//#include<any>
+
+#define variants variant<string, int, float, char, multitype_set>
 
 struct node{
-    //variant<bool, int, float, char, multitype_set> data;
-    any data;
+    variants data;
+    //any data;
     node* link;
 };
 
 class multitype_set{
 public:
     node* head;
-    void set_insert(any el);
-    void set_delete(any el);
-    bool set_search(any el);
+    void set_insert(variants el);
+    void set_delete(variants el);
+    bool set_search(variants el);
     void set_clear();
     multitype_set set_union(multitype_set);
     multitype_set set_union(multitype_set);
