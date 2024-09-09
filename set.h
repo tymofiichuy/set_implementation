bcde554ccb1b4774f6fb5db0b35582780d368296
tymofiichuy <tymofiichuy@gmail.com>
2024-09-09 21:00:18 +0300

    update set.h

--- set.h
+++ set.h
@@ -1,1 +1,24 @@
 #pragma once
+//#include<variant>
+#include<any>
+
+struct node{
+    //variant<bool, int, float, char, multitype_set> data;
+    any data;
+    node* link;
+};
+
+class multitype_set{
+public:
+    node* head;
+    void set_insert(any el);
+    void set_delete(any el);
+    bool set_search(any el);
+    void set_clear();
+    multitype_set set_union(multitype_set);
+    multitype_set set_union(multitype_set);
+    multitype_set set_intersection(multitype_set);
+    multitype_set set_difference(multitype_set);
+    multitype_set set_sym_difference(multitype_set);
+    bool is_subset(multitype_set);
+};
